48d75b004ad01c7870933879f9c5f135106ba6b9
shaohan.hu <shaohan.hu@ride-prostate-rear-asg-shaohan-hu-master-0.ride-prostate-rear-asg-shaohan-hu.wsgo-shaohan-hu.svc.cluster.local>
2023-08-03 08:19:39 +0000

    update visualize.py

--- tools/visualize.py
+++ tools/visualize.py
@@ -1,26 +1,38 @@
 import os
-import tqdm
+from tqdm import tqdm
 import json
+import multiprocessing
 from visual_nuscenes import NuScenes
 use_gt = False
-out_dir = './result_vis/'
-result_json = "work_dirs/pp-nus/results_eval/pts_bbox/results_nusc"
-dataroot='/data/nuscenes'
+res_dir = "test/cfg_self_history/Thu_Aug__3_02_50_25_2023/pts_bbox"
+out_dir = os.path.join(res_dir, 'visualize')
+result_json = os.path.join(res_dir, 'results_nusc')
+dataroot='./data/nuscenes'
 if not os.path.exists(out_dir):
     os.mkdir(out_dir)
 
 if use_gt:
     nusc = NuScenes(version='v1.0-trainval', dataroot=dataroot, verbose=True, pred = False, annotations = "sample_annotation")
 else:
-    nusc = NuScenes(version='v1.0-trainval', dataroot=dataroot, verbose=True, pred = True, annotations = result_json, score_thr=0.25)
+    nusc = NuScenes(version='v1.0-trainval', dataroot=dataroot, verbose=True, pred = True, annotations = result_json, score_thr=0.0)
 
-with open('{}.json'.format(result_json)) as f:
+with open(f"{result_json}.json") as f:
     table = json.load(f)
 tokens = list(table['results'].keys())
 
-for token in tqdm.tqdm(tokens[:100]):
+def vis_token(token):
     if use_gt:
-        nusc.render_sample(token, out_path = "./result_vis/"+token+"_gt.png", verbose=False)
+        nusc.render_sample(token, out_path = f"{out_dir}/"+token+"_gt.png", verbose=False)
     else:
-        nusc.render_sample(token, out_path = "./result_vis/"+token+"_pred.png", verbose=False)
+        nusc.render_sample(token, out_path = f"{out_dir}/"+token+"_pred.png", verbose=False)
+
+vis_cnt = 100
+num_worker = 8
+tokens = tokens[:vis_cnt]
+with multiprocessing.Pool(num_worker) as pool:
+    for item in tqdm(
+        pool.imap(vis_token, tokens),
+        total=len(tokens)
+    ):
+        pass
 
