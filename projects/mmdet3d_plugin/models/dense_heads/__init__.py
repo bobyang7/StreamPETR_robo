710682710982f9f24e1941694fb7560fe5c606c2
吃不胖 <1193322698@qq.com>
2024-05-05 00:53:49 +0800

    add YOLOXHead_Depth_v1

--- projects/mmdet3d_plugin/models/dense_heads/__init__.py
+++ projects/mmdet3d_plugin/models/dense_heads/__init__.py
@@ -8,5 +8,7 @@ from .yolox_head_dep import YOLOXHeadCustomDepth
 from .yolox_head_dep_custom import YOLOXHeadCustomDepth_CUSTOM
 from .sparse_head_custom import SparseHead_CUSTOM
 
+from .yolox_head_dep_v1 import YOLOXHead_Depth_v1
+from .yolox_head_dep_v2 import YOLOXHead_Depth_v2
 from .focal_angle_head_v1 import FocalAngleHead_V1
 from .focal_angle_head_v2 import FocalAngleHead_V2
